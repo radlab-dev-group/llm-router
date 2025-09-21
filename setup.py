98345ad38c43a22ca2123e8fa466dddf6de37a30
Paweł Kędzia <pawel@pradlab.dev>
2025-09-21 21:54:26 +0200

    Change the main module name in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="llms-proxy-api",
+    name="llm-proxy-api",
     version="0.0.1",
     packages=find_packages(exclude=("tests", "docs")),
     python_requires=">=3.10",
