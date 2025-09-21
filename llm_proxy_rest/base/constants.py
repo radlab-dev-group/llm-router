92f60057cade3d865a7e9d78356f8685efd16ef6
Paweł Kędzia <pawel@radlab.dev>
2025-09-22 00:41:19 +0200

    Constants which may be set with the environment.

--- llm_proxy_rest/base/constants.py
+++ llm_proxy_rest/base/constants.py
@@ -1,4 +1,26 @@
+import os
+
 from rdl_ml_utils.utils.env import bool_env_value
 
+
+class _DontChangeMe:
+    MAIN_ENV_PREFIX = "LLM_PROXY_API_"
+
+
+# Directory with predefined system prompts
+PROMPTS_DIR = os.environ.get(
+    f"{_DontChangeMe.MAIN_ENV_PREFIX}PROMPTS_DIR", "resources/prompts"
+)
+
+# Default name of a logging file
+REST_API_LOG_FILE_NAME = os.environ.get(
+    f"{_DontChangeMe.MAIN_ENV_PREFIX}LOG_FILENAME", "llm-proxy-rest.log"
+)
+
+# Default prefix for each endpoint
+DEFAULT_API_PREFIX = os.environ.get(
+    f"{_DontChangeMe.MAIN_ENV_PREFIX}EP_PREFIX", "/api"
+)
+
 # Run service as a proxy only
-SERVICE_AS_PROXY = bool_env_value("LLM_PROXY_SERVICE_MINIMUM")
+SERVICE_AS_PROXY = bool_env_value(f"{_DontChangeMe.MAIN_ENV_PREFIX}MINIMUM")
