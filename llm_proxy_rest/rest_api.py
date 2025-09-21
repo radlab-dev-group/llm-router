a39c4e77da91e549d3c871fd04615bb720e6c95f
Paweł Kędzia <pawel@pradlab.dev>
2025-09-22 00:41:56 +0200

    A little refactor of main app.

--- llm_proxy_rest/rest_api.py
+++ llm_proxy_rest/rest_api.py
@@ -1,30 +1,51 @@
+from typing import List
 from flask import Flask
 
+from llm_proxy_rest.base.constants import (
+    PROMPTS_DIR,
+    REST_API_LOG_FILE_NAME,
+    DEFAULT_API_PREFIX,
+)
 from llm_proxy_rest.endpoints.endpoint_i import EndpointI
 from llm_proxy_rest.register.auto_loader import EndpointAutoLoader
 from llm_proxy_rest.register.register import FlaskEndpointRegistrar
 
 
-def create_app():
-    app = Flask(__name__)
-    registrar = FlaskEndpointRegistrar(app=app, url_prefix="/api")
-
-    loader = EndpointAutoLoader(base_class=EndpointI)
-    classes = loader.discover_classes_in_package("llm_proxy_rest.endpoints")
-    instances = loader.instantiate_without_args(classes)
-
+def __auto_load_endpoints():
+    _aut_loader = EndpointAutoLoader(
+        base_class=EndpointI,
+        prompts_dir=PROMPTS_DIR,
+        logger_file_name=REST_API_LOG_FILE_NAME,
+    )
+    instances = _aut_loader.instantiate_without_args(
+        classes=_aut_loader.discover_classes_in_package("llm_proxy_rest.endpoints")
+    )
     if instances is None or not len(instances):
         raise RuntimeError("No endpoints found!")
 
-    registrar.register_endpoints(endpoints=instances)
+    return instances
 
-    return app
+
+def __register_instances(application, instances: List[EndpointI]):
+    with FlaskEndpointRegistrar(
+        app=application, url_prefix=DEFAULT_API_PREFIX
+    ) as registrar:
+        registrar.register_endpoints(endpoints=instances)
+
+
+def app():
+    flask_app = Flask(__name__)
+    try:
+        __register_instances(
+            application=flask_app, instances=__auto_load_endpoints()
+        )
+    except RuntimeError as e:
+        raise RuntimeError(f"Failed to register endpoints: {e}")
+    return flask_app
 
 
 def main():
-    app = create_app()
-
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app().run(host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == "__main__":
