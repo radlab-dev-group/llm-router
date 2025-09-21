dd251049d9147ac5f3cadec0c9ff2cc1f5c7264a
Paweł Kędzia <pawel@radlab.dev>
2025-09-22 00:40:46 +0200

    EndpointAutoLoader is available with `with` statement.

--- llm_proxy_rest/register/auto_loader.py
+++ llm_proxy_rest/register/auto_loader.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-import logging
 import pkgutil
 import importlib
 
 from typing import Iterable, Dict, Any, Optional, Callable, Type, List, Set
+
+from rdl_ml_utils.utils.logger import prepare_logger
+from rdl_ml_utils.handlers.prompt_handler import PromptHandler
 
 from llm_proxy_rest.endpoints.endpoint_i import EndpointI
 
@@ -19,15 +21,35 @@ class EndpointAutoLoader:
     - from configuration (class path + args/kwargs).
     """
 
-    def __init__(self, base_class: Type[EndpointI]):
+    def __init__(
+        self,
+        base_class: Type[EndpointI],
+        prompts_dir: str,
+        logger_file_name: Optional[str] = None,
+        logger_level: Optional[str] = "DEBUG",
+    ):
         """
         Parameters
         ----------
         base_class : Type[EndpointI]
             The common base class used to discover and type-check endpoints.
+        prompts_dir : str
+            The directory to look for prompts.
+        logger_file_name: str, optional
+            Logger file name, if not given, then will be used default from ml-utils.
+        logger_level: str, optional (default="DEBUG")
+            Logger level. Defaults to "DEBUG".
         """
         self.base_class = base_class
-        self._logger = logging.getLogger(__name__)
+        self.prompts_dir = prompts_dir
+
+        self._prompt_handler = PromptHandler(base_dir=prompts_dir)
+
+        self._logger = prepare_logger(
+            logger_name=__name__,
+            logger_file_name=logger_file_name,
+            log_level=logger_level,
+        )
 
     def discover_classes_in_package(
         self, package_name: str
@@ -43,7 +65,8 @@ class EndpointAutoLoader:
         Parameters
         ----------
         package_name : str
-            Fully qualified package name to search in (e.g., "my_app.endpoints").
+            Fully qualified package name to search in
+            (e.g., "llm_proxy_rest.endpoints").
 
         Returns
         -------
@@ -93,6 +116,7 @@ class EndpointAutoLoader:
         for cls in classes:
             try:
                 instances.append(cls())  # type: ignore[misc]
+                self._logger.warning(f"Instantiating {cls.__name__}")
             except TypeError as e:
                 self._logger.warning(
                     f"Cannot instantiate {cls.__name__} without arguments: {str(e)}"
