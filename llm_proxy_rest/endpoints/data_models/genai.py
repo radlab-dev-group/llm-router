e33f9e0aea448658e3c448d519074d97e066acf8
Paweł Kędzia <pawel@radlab.dev>
2025-09-22 12:54:55 +0200

    Base data models definitions (compatible with llama-service)

---
+++ llm_proxy_rest/endpoints/data_models/genai.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+
+class _GenerativeOptions(BaseModel):
+    temperature: float = 0.75
+    max_new_tokens: int = 256
+
+    top_k: int = 50
+    top_p: float = 0.99
+    typical_p: float = 1.0
+    repetition_penalty: float = 1.2
+
+
+class _GenerativeOptionsModel(_GenerativeOptions):
+    model_name: str
+
+
+class GenerativeConversationModel(_GenerativeOptionsModel):
+    user_last_statement: str
+    historical_messages: List[Dict[str, str]] = []
+
+
+GENAI_REQ_ARGS_BASE = ["model_name"]
+GENAI_OPT_ARGS_BASE = [
+    "max_new_tokens",
+    "top_k",
+    "top_p",
+    "temperature",
+    "typical_p",
+    "repetition_penalty",
+]
+
+GENAI_CONV_REQ_ARGS = GENAI_REQ_ARGS_BASE + ["user_last_statement"]
+GENAI_CONV_OPT_ARGS = GENAI_OPT_ARGS_BASE
