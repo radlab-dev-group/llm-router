129507b613268436aa04c66121d1773428c5acb3
Paweł Kędzia <pawel@radlab.dev>
2025-10-11 21:52:20 +0200

    Rename package from **llm‑proxy‑api** to **llm‑router**.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="llm-proxy-api",
+    name="llm-router",
     version="0.1.0",
     author="RadLab team",
     packages=find_packages(exclude=("tests", "docs")),
