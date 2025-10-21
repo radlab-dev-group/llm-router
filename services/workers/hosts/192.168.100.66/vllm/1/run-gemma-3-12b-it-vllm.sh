03eaa60545c83618b63de9f6f31e81e63bea5f82
Paweł Kędzia <pawel@radlab.dev>
2025-10-22 01:12:33 +0200

    Update Gemma‑3‑12B‑IT v.66 host ascripts

---
+++ services/workers/hosts/192.168.100.66/vllm/1/run-gemma-3-12b-it-vllm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export CUDA_VISIBLE_DEVICES=1
+
+MODEL_PATH=google/gemma-3-12b-it
+
+vllm serve \
+        "${MODEL_PATH}" \
+        --port 7001 \
+        --host 0.0.0.0 \
+        --quantization bitsandbytes \
+        --load-format bitsandbytes \
+        --max-model-len=56000 \
+        --max_num_seqs=8 \
+        --gpu-memory-utilization=0.90
