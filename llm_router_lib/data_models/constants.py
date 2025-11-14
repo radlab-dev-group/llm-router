4cf90276ef5608d8615be32b53ac2af500e9f067
Paweł Kędzia <pawel@radlab.dev>
2025-11-14 04:39:08 +0100

    Comment out unused typing import and disable the `POSSIBLE_FIELDS_WITH_TEXTS` mapping.

--- llm_router_lib/data_models/constants.py
+++ llm_router_lib/data_models/constants.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any
+# from typing import List, Dict, Any
 
 LANGUAGE_PARAM = "language"
 SYSTEM_PROMPT = "system_prompt"
@@ -6,22 +6,22 @@ MODEL_NAME_PARAM = "model_name"
 
 MODEL_NAME_PARAMS = [MODEL_NAME_PARAM, "model"]
 
-POSSIBLE_FIELDS_WITH_TEXTS = {
-    # Text-like
-    "text": str,
-    "text_str": str,
-    "language": str,
-    "user_query": str,
-    "article_type": str,
-    "question_str": str,
-    "user_last_statement": str,
-    # Prompt messages
-    "system_prompt" "": str,
-    "question_prompt": str,
-    # Model name
-    "model_name": str,
-    # Dict like: k and v will be anonymized
-    "messages": List[Dict[str, Any]],
-    "texts": Dict[str, List[str]] | List[str],
-    "historical_messages": List[Dict[str, str]],
-}
+# POSSIBLE_FIELDS_WITH_TEXTS = {
+#     # Text-like
+#     "text": str,
+#     "text_str": str,
+#     "language": str,
+#     "user_query": str,
+#     "article_type": str,
+#     "question_str": str,
+#     "user_last_statement": str,
+#     # Prompt messages
+#     "system_prompt" "": str,
+#     "question_prompt": str,
+#     # Model name
+#     "model_name": str,
+#     # Dict like: k and v will be anonymized
+#     "messages": List[Dict[str, Any]],
+#     "texts": Dict[str, List[str]] | List[str],
+#     "historical_messages": List[Dict[str, str]],
+# }
