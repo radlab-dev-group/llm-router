e642410212373276d49b4221011758f6390033a2
Paweł Kędzia <pawel@pradlab.dev>
2025-11-14 01:49:16 +0100

    Add anonymizer package: core orchestrator, rule interface, base rule, concrete rule imports, PESEL validator, utilities, and README documentation

---
+++ llm_router_lib/anonymizer/__init__.py
@@ -0,0 +1,8 @@
+"""
+Top‑level package for the anonymizer library.
+
+The public API currently consists of:
+- Anonymizer (core)
+- AnonymizeRuleI (interface)
+- Concrete rule implementations (PhoneRule, UrlRule, IpRule, PeselRule)
+"""
---
+++ llm_router_lib/anonymizer/core/__init__.py
@@ -0,0 +1,6 @@
+"""
+Core components of the anonymizer library.
+"""
+
+from llm_router_lib.anonymizer.core.anonymizer import Anonymizer
+from llm_router_lib.anonymizer.core.rule_interface import AnonymizeRuleI
