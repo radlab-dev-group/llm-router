43bfa66e1e88ca82bfd9ddd053c135f44281d55f
Paweł Kędzia <pawel@pradlab.dev>
2025-11-15 13:37:09 +0100

    **Remove obsolete anonymizer `__init__` modules and tidy constants file** - Delete `llm_router_lib/anonymizer/core/__init__.py` and `llm_router_lib/anonymizer/rules/__init__.py` as they are no longer needed. - Ensure `USE_BETA_FEATURES` line in `constants.py` ends with a proper newline.

--- llm_router_lib/anonymizer/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Core components of the anonymizer library.
-"""
-
-from llm_router_lib.anonymizer.core.anonymizer import Anonymizer
-from llm_router_lib.anonymizer.core.rule_interface import AnonymizeRuleI
