9d5f19db1d1770e032d1ee538c18910bd9651e06
Paweł Kędzia <pawel@radlab.dev>
2025-11-15 13:26:15 +0100

    Rename anonymizer package to FastMasker plugin and switch core classes to use `MaskerRuleI` interface.

--- llm_router_lib/anonymizer/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Top‑level package for the anonymizer library.
-
-The public API currently consists of:
-- Anonymizer (core)
-- AnonymizeRuleI (interface)
-- Concrete rule implementations (PhoneRule, UrlRule, IpRule, PeselRule)
-"""
