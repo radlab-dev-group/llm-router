815adad309afca5b093c85876eb443346a45fffc
Paweł Kędzia <pawel@pradlab.dev>
2025-11-17 03:31:26 +0100

    Add streaming support in OpenAI client test script and update 0.2.4 changelog to note `FORCE_ANONYMISATION` mode.

--- tests/openai-client.py
+++ tests/openai-client.py
@@ -8,9 +8,21 @@ client = OpenAI(
     base_url="http://192.168.100.65:8080/v1",
 )
 
+use_stream = True
+
+model_1 = "google/gemma-3-12b-it"
+model_2 = "gpt-oss:120b"
+
 response = client.chat.completions.create(
-    model="google/gemma-3-12b-it",
-    messages=[{"role": "user", "content": "Hello world"}],
+    model=model_1,
+    messages=[{"role": "user", "content": "Write simple somethig"}],
+    stream=use_stream,
 )
 
-print(response.choices)
+if use_stream:
+    for s in response:
+        if not s:
+            break
+        print(s.choices[0].delta.content, end="")
+else:
+    print(response.choices)
