a39b44e2763c566577e2fb56f1cfe1299af8d90f
Paweł Kędzia <pawel@radlab.dev>
2025-12-14 19:00:59 +0100

    Add TranslateTextService and `client.translate` method; rename base conversation service to `BaseConversationServiceInterface` and introduce `NoArgsAndNoPayloadError`.

--- llm_router_lib/client.py
+++ llm_router_lib/client.py
@@ -1,15 +1,18 @@
 import logging
-from typing import Optional, Dict, Any, Union
+from typing import Optional, Dict, Any, Union, List
 
-from llm_router_lib.data_models.builtin_chat import (
-    GenerativeConversationModel,
-    ExtendedGenerativeConversationModel,
-)
+from llm_router_lib.utils.http import HttpRequester
+from llm_router_lib.exceptions import NoArgsAndNoPayloadError
+from llm_router_lib.services.utils import TranslateTextService
+from llm_router_lib.data_models.builtin_utils import TranslateTextModel
 from llm_router_lib.services.conversation import (
     ConversationService,
     ExtendedConversationService,
 )
-from llm_router_lib.utils.http import HttpRequester
+from llm_router_lib.data_models.builtin_chat import (
+    GenerativeConversationModel,
+    ExtendedGenerativeConversationModel,
+)
 
 
 class LLMRouterClient:
@@ -58,3 +61,28 @@ class LLMRouterClient:
         if isinstance(payload, ExtendedGenerativeConversationModel):
             payload = payload.model_dump()
         return ExtendedConversationService(self.http, self.logger).call(payload)
+
+    # ------------------------------------------------------------------ #
+    def translate(
+        self,
+        payload: Optional[
+            Union[
+                Dict[str, Any],
+                TranslateTextModel,
+            ]
+        ] = None,
+        texts: Optional[List[str]] = None,
+        model: Optional[str] = None,
+    ) -> Dict[str, Any]:
+        if isinstance(payload, TranslateTextModel):
+            payload = payload.model_dump()
+        elif isinstance(payload, Dict):
+            payload = payload
+        else:
+            if not texts or not model:
+                raise NoArgsAndNoPayloadError(
+                    "No payload and no arguments were passed!"
+                )
+            payload = TranslateTextModel(model_name=model, texts=texts).model_dump()
+
+        return TranslateTextService(self.http, self.logger).call(payload)
