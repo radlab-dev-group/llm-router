ff8aa47c9981d41d651cb2fd10fb291cdce65422
pawkedzia <pawel@radlab.dev>
2026-09-06 04:57:07 +0200

    **Test:** Add coverage-enabled test runs for `llm_router_lib` and `llm_router_cli` folders

--- check-dev.sh
+++ check-dev.sh
@@ -15,3 +15,5 @@ mypy ./llm_router_lib
 bandit -r .
 
 pytest llm_router_api/tests --cov=llm_router_api -q
+pytest llm_router_lib/tests --cov=llm_router_lib -q
+pytest llm_router_cli/tests --cov=llm_router_cli -q
