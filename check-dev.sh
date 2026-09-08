bffb9f51b9308c78cb658f0739bd72c08e5921c4
pawkedzia <pawel@radlab.dev>
2026-09-08 12:08:56 +0200

    **Dev:** Exclude `tests` directory from `mypy` checks; remove unnecessary pytest flags in `check-dev.sh`

--- check-dev.sh
+++ check-dev.sh
@@ -8,12 +8,12 @@ pylint ./llm_router_api --ignore=tests
 pylint ./llm_router_cli --ignore=tests
 pylint ./llm_router_lib --ignore=tests
 
-mypy ./llm_router_api
-mypy ./llm_router_cli
-mypy ./llm_router_lib
+mypy ./llm_router_api --exclude=tests
+mypy ./llm_router_cli --exclude=tests
+mypy ./llm_router_lib --exclude=tests
 
 bandit -r .
 
-pytest llm_router_api/tests --cov=llm_router_api -q
-pytest llm_router_lib/tests --cov=llm_router_lib -q
-pytest llm_router_cli/tests --cov=llm_router_cli -q
+pytest llm_router_api/tests
+pytest llm_router_lib/tests
+pytest llm_router_cli/tests
