976f4d0fd00b4d9b69a48ef917aeee35971b58bd
pawkedzia <pawel@radlab.dev>
2026-09-08 12:10:51 +0200

    **Dev:** Update `bandit` checks in `check-dev.sh` to exclude `tests` directories for all components

--- check-dev.sh
+++ check-dev.sh
@@ -12,7 +12,10 @@ mypy ./llm_router_api --exclude=tests
 mypy ./llm_router_cli --exclude=tests
 mypy ./llm_router_lib --exclude=tests
 
-bandit -r .
+bandit -r llm_router_api --exclude llm_router_api/tests
+bandit -r llm_router_cli --exclude llm_router_cli/tests
+bandit -r llm_router_lib --exclude llm_router_lib/tests
+
 
 pytest llm_router_api/tests
 pytest llm_router_lib/tests
