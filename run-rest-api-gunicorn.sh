872b3376bb7b741663db88cbd37a965583d8f270
pawkedzia <pawel@radlab.dev>
2026-09-16 22:50:15 +0200

    **CLI:** Update `server` command to use `-i localhost-dev` for the default instance

--- run-rest-api-gunicorn.sh
+++ run-rest-api-gunicorn.sh
@@ -188,4 +188,4 @@ export TOKENIZERS_PARALLELISM=${TOKENIZERS_PARALLELISM:-true}
 # ==================================================================================
 # RUN MAIN APPLICATION
 # ==================================================================================
-llm-router server start --foreground
+llm-router server start -i localhost-dev --foreground
